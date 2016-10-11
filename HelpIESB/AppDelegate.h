4e162df5e17f67eaf9c835382840fec01c1b6f6d
Gilvan Souza <gilvan@MacBook-Pro-de-Gilvan.local>
2016-10-11 10:42:43 -0300

    Initial Commit

---
+++ HelpIESB/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  HelpIESB
+//
+//  Created by Gilvan Souza on 11/10/16.
+//  Copyright © 2016 Gilvan Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
